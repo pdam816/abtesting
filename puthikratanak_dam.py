a4f5d4f3f2b70be275cb47a59cc086d2752b5f85
pdam816 <44200279+pdam816@users.noreply.github.com>
2020-10-29 02:00:46 -0400

    renamed abtesting.py to puthikratanak_dam.py

--- puthikratanak_dam.py
+++ puthikratanak_dam.py
@@ -158,7 +158,6 @@ def get_expected_grid(observed_grid):
 
     num_rows = len(observed_grid)
     num_col = len(observed_grid[0])
-
     expected_grid = [[0 for i in range(num_col)] for j in range(num_rows)]
 
     for row in range(num_rows):
@@ -220,45 +219,92 @@ def data_to_num_list(s):
 
 
 # t_test 1:
-a_t1_list = data_to_num_list(a1)
-b_t1_list = data_to_num_list(b1)
-print(get_t_score(a_t1_list, b_t1_list))  # this should be -129.500
-print(perform_2_sample_t_test(a_t1_list, b_t1_list))  # this should be 0.0000
-# why do you think this is? Take a peek at a1 and b1 in abtesting_test.py :)
+# a_t1_list = data_to_num_list(a1)
+# b_t1_list = data_to_num_list(b1)
+# print(get_t_score(a_t1_list, b_t1_list))  # this should be -129.500
+# print(perform_2_sample_t_test(a_t1_list, b_t1_list))  # this should be 0.0000
+# # why do you think this is? Take a peek at a1 and b1 in abtesting_test.py :)
 
-# t_test 2:
-a_t2_list = data_to_num_list(a2)
-b_t2_list = data_to_num_list(b2)
-print(get_t_score(a_t2_list, b_t2_list))  # this should be -1.48834
-print(perform_2_sample_t_test(a_t2_list, b_t2_list))  # this should be .082379
+# # t_test 2:
+# a_t2_list = data_to_num_list(a2)
+# b_t2_list = data_to_num_list(b2)
+# print(get_t_score(a_t2_list, b_t2_list))  # this should be -1.48834
+# print(perform_2_sample_t_test(a_t2_list, b_t2_list))  # this should be .082379
 
-# t_test 3:
-a_t3_list = data_to_num_list(a3)
-b_t3_list = data_to_num_list(b3)
-print(get_t_score(a_t3_list, b_t3_list))  # this should be -2.88969
-print(perform_2_sample_t_test(a_t3_list, b_t3_list))  # this should be .005091
+# # t_test 3:
+# a_t3_list = data_to_num_list(a3)
+# b_t3_list = data_to_num_list(b3)
+# print(get_t_score(a_t3_list, b_t3_list))  # this should be -2.88969
+# print(perform_2_sample_t_test(a_t3_list, b_t3_list))  # this should be .005091
 
 
-# chi2_test 1:
-a_c1_list = data_to_num_list(a_count_1)
-b_c1_list = data_to_num_list(b_count_1)
-c1_observed_grid = [a_c1_list, b_c1_list]
-print(chi2_value(c1_observed_grid))  # this should be 4.103536
-# this should be .0427939
-print(perform_chi2_homogeneity_test(c1_observed_grid))
+# # chi2_test 1:
+# a_c1_list = data_to_num_list(a_count_1)
+# b_c1_list = data_to_num_list(b_count_1)
+# c1_observed_grid = [a_c1_list, b_c1_list]
+# print(chi2_value(c1_observed_grid))  # this should be 4.103536
+# # this should be .0427939
+# print(perform_chi2_homogeneity_test(c1_observed_grid))
 
-# chi2_test 2:
-a_c2_list = data_to_num_list(a_count_2)
-b_c2_list = data_to_num_list(b_count_2)
-c2_observed_grid = [a_c2_list, b_c2_list]
-print(chi2_value(c2_observed_grid))  # this should be 33.86444
-print(perform_chi2_homogeneity_test(c2_observed_grid))  # this should be 0.0000
-# Again, why do you think this is? Take a peek at a_count_2 and b_count_2 in abtesting_test.py :)
+# # chi2_test 2:
+# a_c2_list = data_to_num_list(a_count_2)
+# b_c2_list = data_to_num_list(b_count_2)
+# c2_observed_grid = [a_c2_list, b_c2_list]
+# print(chi2_value(c2_observed_grid))  # this should be 33.86444
+# print(perform_chi2_homogeneity_test(c2_observed_grid))  # this should be 0.0000
+# # Again, why do you think this is? Take a peek at a_count_2 and b_count_2 in abtesting_test.py :)
 
-# chi2_test 3:
-a_c3_list = data_to_num_list(a_count_3)
-b_c3_list = data_to_num_list(b_count_3)
-c3_observed_grid = [a_c3_list, b_c3_list]
-print(chi2_value(c3_observed_grid))  # this should be .3119402
-# this should be .57649202
-print(perform_chi2_homogeneity_test(c3_observed_grid))
+# # chi2_test 3:
+# a_c3_list = data_to_num_list(a_count_3)
+# b_c3_list = data_to_num_list(b_count_3)
+# c3_observed_grid = [a_c3_list, b_c3_list]
+# print(chi2_value(c3_observed_grid))  # this should be .3119402
+# # this should be .57649202
+# print(perform_chi2_homogeneity_test(c3_observed_grid))
+
+
+a_time_to_comp = """
+48072
+10278
+297464
+8545
+8644
+6531
+7276
+3600
+6140
+3945
+11562
+35796
+6806
+19703
+"""
+
+b_time_to_comp = """
+6230
+9952
+20609
+180205
+106540
+3600
+18659
+5172
+35652
+106806
+"""
+
+a_count = """8	6"""
+b_count = """6	4"""
+
+# run metrics for t-test
+list_a = data_to_num_list(a_time_to_comp)
+list_b = data_to_num_list(b_time_to_comp)
+print("t-score:", get_t_score(list_a, list_b))  # this should be -129.500
+print("t-test:", perform_2_sample_t_test(list_a, list_b))
+
+# run metrics for chi2 test
+a_count_list = data_to_num_list(a_count)
+b_count_list = data_to_num_list(b_count)
+c1_observed_grid = [a_count_list, b_count_list]
+print("chi2 value:", chi2_value(c1_observed_grid))  # this should be 4.103536
+print("chi2-test:", perform_chi2_homogeneity_test(c1_observed_grid))
